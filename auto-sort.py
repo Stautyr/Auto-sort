998e8c77a8d3388bd55f2ad107381d5c6f4464af
Stautyr <47163082+Stautyr@users.noreply.github.com>
2020-08-17 14:22:53 -0500

    Update auto-sort.py

--- auto-sort.py
+++ auto-sort.py
@@ -14,13 +14,13 @@ import shutil
 import send2trash
 from pathlib import Path
 
-DEBUG = False
+DEBUG = True
 
 #User of system
-user = "[ENTER USER HERE]"
+user = "lacro"
 
 #Enable if OneDrive is active
-OneDrive = False
+OneDrive = True
 
 #OneDrive check
 startPath = Path("/Users/"+user)
@@ -39,104 +39,61 @@ solidworks = DocPath/"SOLIDWORKS"
 videoSort = startPath/"Videos"
 
 #folder assignment
-doc = docSort/"DOC FILES"
-exe = docSort/"EXE FILES"
-jpg = picSort/"JPG"
-lvix =  videoSort/"LVIX"
-mov = videoSort/"Captures"
-mp4 = musicSort/"MP4"
-other = docSort/"OTHER"
-pdf = docSort/"PDFs"
-placeholder = "Not setup yet"
-png = picSort/"PNG"
-pptx = docSort/"PPTX FILES"
-psarc = docSort/"PSARC FILES"
-py = compSci/"Python Scripts/Download Sort"
-sldasm = solidworks/"SOLIDWORKS Assembly"
-sldprt = solidworks/"SOLIDWORKS Parts"
-txt = docSort/"TXT FILES"
-wav = musicSort/"WAV"
-zipFolder = docSort/"ZIP FILES"
+folders = {"doc" : docSort/"DOC FILES",
+		   "exe" : docSort/"EXE FILES",
+		   "jpg" : picSort/"JPG",
+		   "lvix" :  videoSort/"LVIX",
+		   "mov" : videoSort/"Captures",
+		   "mp4" : musicSort/"MP4",
+		   "other" : docSort/"OTHER",
+		   "pdf" : docSort/"PDFs",
+		   "png" : picSort/"PNG",
+		   "pptx" : docSort/"PPTX FILES",
+		   "psarc" : docSort/"PSARC FILES",
+		   "py" : compSci/"Python Scripts/Download Sort",
+		   "sldasm" : solidworks/"SOLIDWORKS Assembly",
+		   "sldprt" : solidworks/"SOLIDWORKS Parts",
+		   "txt" : docSort/"TXT FILES",
+		   "wav" : musicSort/"WAV",
+		   "zipFolder" : docSort/"ZIP FILES"}
 
 #Checks for folders and creates them if they do not exist 
 def check():
-    folders = [doc, exe, jpg, lvix, mov, mp4, other, pdf, png, pptx, psarc, py, sldasm, sldprt, txt, wav, zipFolder]
-    for count in range(len(folders)):
-                 if Path(folders[count]).is_dir():
-                     if(DEBUG):
-                        print ("{} exist".format(folders[count]))
-                 else:
-                     if(DEBUG):
-                         print ("{} does not exist".format(folders[count]))
-                         print ("Creating {}".format(folders[count]))
-                     os.mkdir(folders[count])
+    for count in folders:
+    	if Path(folders[count]).is_dir():
+    		#if(DEBUG):
+    			#print ("{} exist".format(folders[count]))
+    		pass
+    	else:
+            if(DEBUG):
+                print ("{} does not exist".format(folders[count]))
+                print ("Creating {}".format(folders[count]))
+            os.mkdir(folders[count])
 
 #Sorting functions
 def send(file, ext):
     """Sends files to their sorted locations based on the file extension. 
     If not extension is not listed then it is sent to other by default."""
-    if(ext == ".doc" or ext == ".docx"):
-        folder = doc
-
-    elif(ext == ".exe"):
-        folder = exe
-
-    elif(ext == ".jpg"):
-        folder = jpg
-
-    elif(ext == ".lvix"):
-        folder = lvix
-
-    elif(ext == ".mov"):
-        folder = mov
-
-    elif(ext == ".mp3" or ext == ".m4a" or ext == ".wma"):
-        folder = songSort(startFolder/file, ext)
-
-    elif(ext == ".mp4"):
-        folder = mp4
-
-    elif(ext == ".pdf"):
-        folder = pdf
-
-    elif(ext == ".png"):
-        folder = png
-
-    elif(ext == ".pptx"):
-        folder = pptx
-
-    elif(ext == ".psarc"):
-        folder = psarc
-        
-    elif(ext == ".py"):
-        folder = py
-
-    elif(ext == ".sldasm"):
-        folder = sldasm
-        
-    elif(ext == ".sldprt"):
-        folder = sldprt
-
-    elif(ext == ".txt"):
-        folder = txt
-
-    elif(ext == ".wav"):
-        folder = wav
-   
-    elif(ext == ".zip"):
-        folder = zipFolder
-        
-    else:
-        print ("Other file type found: " + ext)
-        folder = other    
+    ext = ext[1:]
+    try:
+    	folder = folders[ext]
+    	if(ext == "docx"):
+    		folder = folders["doc"]
+    	
+    	elif(ext == "mp3" or ext == "m4a" or ext == "wma"):
+        	folder = songSort(startFolder/file, ext)
     
-    if(folder == placeholder):
-        print ("Cannot send '{}'. File path not setup yet".format(file))
-        
-    else:
-        if (DEBUG):
-            print("Sending {} to {}".format(file,folder))
-        shutil.move(startFolder/file, folder/file)        
+    except:
+    	print ("Other file type found: " + ext)
+    	folder = folders["other"]
+    	if (DEBUG):
+    		print("Sending {} to {}".format(file,folder))
+    	shutil.move(startFolder/file, folder/file)
+    
+    else:    
+    	if (DEBUG):
+    		print("Sending {} to {}".format(file,folder))
+    	shutil.move(startFolder/file, folder/file)        
 
 def songSort(song, ext):
     if(ext == ".mp3"):
@@ -192,4 +149,3 @@ for x in range(len(o)):
     if(DEBUG):
         print(ext)
     send(file, ext)                          #sorts file
-
