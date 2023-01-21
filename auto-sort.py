5384cb558da4d4e6ab65076ecfd5e2397ebdcf89
Stautyr <47163082+Stautyr@users.noreply.github.com>
2023-01-21 00:42:23 -0600

    Added xlsx
    Simple reformatting and xlsx addition

--- auto-sort.py
+++ auto-sort.py
@@ -16,19 +16,19 @@ from pathlib import Path
 
 DEBUG = False
 if (DEBUG):
-	print("IN DEBUGGING MODE")
-	#User of system
-	user = "[INSERT USER HERE]"
+        print("IN DEBUGGING MODE")
+        #User of system
+        user = "[INSERT USER HERE]"
 
-	#Enable if OneDrive is active
-	OneDrive = True
+        #Enable if OneDrive is active
+        OneDrive = True
 
 else:
-        OneDrive = False
+        OneDrive = True
         pathList = str(Path.cwd()).split("\\")
         user = pathList[2]
-        if (pathList[3] == "OneDrive"):
-                OneDrive = True
+        # if (pathList[3] == "OneDrive"):
+        #        OneDrive = True
 
         #OneDrive check
         startPath = Path("/Users/"+user)
@@ -46,58 +46,60 @@ picSort = startPath/"Pictures/Camera Roll"
 videoSort = startPath/"Videos"
 
 #folder assignment
-folders = {"doc" : docSort/"DOC FILES",
-	   "exe" : docSort/"EXE FILES",
-	   "jpg" : picSort/"JPG",
-	   "mov" : videoSort/"Captures",
-	   "mp4" : musicSort/"MP4",
-	   "other" : docSort/"OTHER",
-	   "pdf" : docSort/"PDFs",
-	   "png" : picSort/"PNG",
-	   "pptx" : docSort/"PPTX FILES",
-	   "txt" : docSort/"TXT FILES",
-	   "wav" : musicSort/"WAV",
-	   "zip" : docSort/"ZIP FILES"}
+folders = {
+        "doc" : docSort/"DOC FILES",
+        "exe" : docSort/"EXE FILES",
+        "xlsx": docSort/"EXCEL FILES",
+        "jpg" : picSort/"JPG",
+        "mov" : videoSort/"Captures",
+        "mp4" : musicSort/"MP4",
+        "other" : docSort/"OTHER",
+        "pdf" : docSort/"PDFs",
+        "png" : picSort/"PNG",
+        "pptx" : docSort/"PPTX FILES",
+        "txt" : docSort/"TXT FILES",
+        "wav" : musicSort/"WAV",
+        "zip" : docSort/"ZIP FILES"}
 
 #Checks for folders and creates them if they do not exist
 def check():
-	for count in folders:
-		if Path(folders[count]).is_dir():
-			if(DEBUG):
-				print ("{} exist".format(folders[count]))
-			pass
-		else:
-			if(DEBUG):
-				print ("{} does not exist".format(folders[count]))
-				print ("Creating {}".format(folders[count]))
-			os.mkdir(folders[count])
+        for count in folders:
+                if Path(folders[count]).is_dir():
+                        if(DEBUG):
+                                print ("{} exist".format(folders[count]))
+                        pass
+                else:
+                        if(DEBUG):
+                                print ("{} does not exist".format(folders[count]))
+                                print ("Creating {}".format(folders[count]))
+                        os.mkdir(folders[count])
 
 def songSort(song, ext):
-    if(ext == "mp3"):
-        metadata = EasyID3(song)
-        album = str(metadata['album'])
+        if(ext == "mp3"):
+                metadata = EasyID3(song)
+                album = str(metadata['album'])
 
-    elif(ext == "m4a"):
-        metadata = MP4(song)
-        album = str(metadata["\xa9alb"])
+        elif(ext == "m4a"):
+                metadata = MP4(song)
+                album = str(metadata["\xa9alb"])
 
-    elif(ext == "wma"):
-        metadata = ASF(song)
-        album = str(metadata["WM/AlbumTitle"])
-        album = album[22:-3]
+        elif(ext == "wma"):
+                metadata = ASF(song)
+                album = str(metadata["WM/AlbumTitle"])
+                album = album[22:-3]
 
-    else:
-        album = album[2:-2]
+        else:
+                album = album[2:-2]
 
-    albumList = os.listdir(musicSort/"Albums")
+        albumList = os.listdir(musicSort/"Albums")
 
-    #album folder check
-    if (album not in albumList):
-        if(DEBUG):
-            print(song)
-            print(album + " not found")
+        #album folder check
+        if (album not in albumList):
+                if(DEBUG):
+                        print(song)
+                        print(album + " not found")
         os.mkdir(musicSort/"Albums"/album)
-    return(musicSort/"Albums"/album)
+        return(musicSort/"Albums"/album)
 
 #Sorting functions
 def send(file, ext):
@@ -133,12 +135,12 @@ def send(file, ext):
 
 
 def delEmpty(startFolder):
-	for folderName, subfolders, filenames in os.walk(startFolder):
-		folderSize = os.path.getsize(folderName)
-		if (folderSize == 0):
-			if (DEBUG):
-				print(folderName + " is empty \n deleting " + folderName)
-			send2trash.send2trash(folderName)
+        for folderName, subfolders, filenames in os.walk(startFolder):
+                folderSize = os.path.getsize(folderName)
+                if (folderSize == 0):
+                        if (DEBUG):
+                                print(folderName + " is empty \n deleting " + folderName)
+                        send2trash.send2trash(folderName)
 
 ###########################################################################################
 #                                        MAIN PROGRAM                                     #
@@ -153,8 +155,11 @@ startFolder = downloads
 #Sorting method
 o = os.listdir(startFolder)                  #creates list of all files in starting folder
 for x in range(len(o)):
-	file = o[x]
-	ext = os.path.splitext(file)[-1].lower() #isolates file extension
-	if(DEBUG):
-		print(ext)
-	send(file, ext)                          #sorts file
+        file = o[x]
+        ext = os.path.splitext(file)[-1].lower() #isolates file extension
+        if(DEBUG):
+                print(ext)
+        try:
+                send(file, ext)                          #sorts file
+        except PermissionError:
+                pass
